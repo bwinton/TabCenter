8074ab3fdbc461f9352c1cf43267bbac327b6740
Philipp von Weitershausen <philipp@weitershausen.de>
2010-03-21 22:39:10 +0000

    First stab at making tabs vertical... Things can be so simple :)

--- content/verticaltabs.js
+++ content/verticaltabs.js
@@ -1,21 +1,34 @@
-var verticaltabs = {
-  onLoad: function() {
-    // initialization code
-    this.initialized = true;
-    this.strings = document.getElementById("verticaltabs-strings");
-  },
+var VerticalTabs = {
 
-  onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
-  },
+    handleEvent: function(event) {
+        switch (event.type) {
+        case 'DOMContentLoaded':
+            this.init();
+            return;
+        }
+    },
 
-  onToolbarButtonCommand: function(e) {
-    // just reuse the function above.  you can change this, obviously!
-    verticaltabs.onMenuItemCommand(e);
-  }
+    init: function() {
+        window.removeEventListener("DOMContentLoaded", this, false);
+
+        // Move the tabs next to the tabbrowser, make them vertical
+        // and place a splitter between them.
+
+        var tabs = document.getElementById("tabbrowser-tabs");
+        var contentbox = document.getElementById("appcontent");
+        contentbox.orient = "horizontal";
+        contentbox.insertBefore(tabs, contentbox.lastChild);
+
+        tabs.orient = "vertical";
+        tabs.mTabstrip.orient = "vertical";
+        tabs.tabbox.orient = "horizontal"; // probably not necessary
+
+        var splitter = document.createElement("splitter");
+        splitter.setAttribute("id", "verticaltabs-splitter");
+        splitter.setAttribute("class", "chromeclass-extrachrome");
+        contentbox.insertBefore(splitter, contentbox.lastChild);
+    }
+
 };
 
-window.addEventListener("load", verticaltabs.onLoad, false);
+window.addEventListener("DOMContentLoaded", VerticalTabs, false);
