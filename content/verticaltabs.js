3228a30835f8958071bd8af60e05b113bf18b74a
Philipp von Weitershausen <philipp@weitershausen.de>
2010-03-21 22:58:15 +0000

    Don't move the tabs next to the tabbrowser, move it one level higher up, next to the sidebar.  That way we can move the bottom stuff (statusbar, findbar, etc.) in with the tabbrowser.

--- content/verticaltabs.js
+++ content/verticaltabs.js
@@ -11,14 +11,18 @@ var VerticalTabs = {
     init: function() {
         window.removeEventListener("DOMContentLoaded", this, false);
 
-        // Move the tabs next to the tabbrowser, make them vertical
-        // and place a splitter between them.
-
-        var tabs = document.getElementById("tabbrowser-tabs");
+        // Move the bottom stuff (statusbar, findbar) in with the
+        // tabbrowser.  That way it will share the same (horizontal)
+        // space as the brower.  In other words, the bottom stuff no
+        // longer extends across the whole bottom of the window.
         var contentbox = document.getElementById("appcontent");
-        contentbox.orient = "horizontal";
-        contentbox.insertBefore(tabs, contentbox.lastChild);
+        var bottom = document.getElementById("browser-bottombox");
+        contentbox.appendChild(bottom);
 
+        // Move the tabs next to the app content and make them vertical
+        // and place a splitter between them.
+        var tabs = document.getElementById("tabbrowser-tabs");
+        contentbox.parentNode.insertBefore(tabs, contentbox);
         tabs.orient = "vertical";
         tabs.mTabstrip.orient = "vertical";
         tabs.tabbox.orient = "horizontal"; // probably not necessary
@@ -26,7 +30,8 @@ var VerticalTabs = {
         var splitter = document.createElement("splitter");
         splitter.setAttribute("id", "verticaltabs-splitter");
         splitter.setAttribute("class", "chromeclass-extrachrome");
-        contentbox.insertBefore(splitter, contentbox.lastChild);
+        contentbox.parentNode.insertBefore(splitter, contentbox);
+
     }
 
 };
