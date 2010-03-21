9c8d6b255c0253b4dd6a205feb6047d25f63e5ca
Philipp von Weitershausen <philipp@weitershausen.de>
2010-03-21 22:42:55 +0000

    Some general cleanup

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,2 @@
-pref("extensions.verticaltabs.boolpref", false);
-pref("extensions.verticaltabs.intpref", 0);
-pref("extensions.verticaltabs.stringpref", "A string");
-
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.verticaltabs@philikon.de.description", "chrome://verticaltabs/locale/overlay.properties");
