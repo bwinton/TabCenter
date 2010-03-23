e960b1afb334374cb6c7cf7a6084dc8e49c913f3
Philipp von Weitershausen <philipp@weitershausen.de>
2010-03-23 02:39:16 +0000

    This default is no longer necessary as we hide the original all tabs button anyhoo

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,5 +1,3 @@
-// The regular "All Tabs" button doesn't really make sense with vertical tabs
-pref("browser.allTabs.previews", true);
 pref("extensions.verticaltabs.width", 250);
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
