8be2d4c9b9ca7dc42332a1e037288b89275724bf
Philipp von Weitershausen <philipp@weitershausen.de>
2010-03-24 00:30:44 +0000

    Allow multiple tabs to be selected, using Ctrl+click (on the Mac it's Cmd+Click) as well as Shift+Click.

--- content/verticaltabs.js
+++ content/verticaltabs.js
@@ -38,6 +38,10 @@ var VerticalTabs = {
             = TabbrowserTabs._setEffectAllowedForDataTransfer;
         tabs.addEventListener('dragover', this, false);
 
+        // Multiselect
+        tabs.addEventListener('mousedown', this, true);
+        tabs.addEventListener('TabSelect', this, false);
+
         // Fix up each individual tab for vertical layout, including
         // ones that are opened later on.
         tabs.addEventListener('TabOpen', this, true);
@@ -65,6 +69,31 @@ var VerticalTabs = {
             }, 10, this);
 	},
 
+    multiSelect: function(aTab) {
+        // Clear the multiselect flag from the selected tab
+        aTab.setAttribute("multiselect", "true");
+    },
+
+    multiSpanSelect: function(aBeginTab, aEndTab) {
+        this.clearMultiSelect();
+        var tabs = document.getElementById("tabbrowser-tabs");
+        var begin = aBeginTab._tPos;
+        var end = aEndTab._tPos;
+        if (begin > end) {
+            [end, begin] = [begin, end];
+        }
+        for (let i=begin; i <= end; i++) {
+            this.multiSelect(tabs.childNodes[i]);
+        }
+    },
+
+    clearMultiSelect: function() {
+        var tabs = document.getElementById("tabbrowser-tabs");
+        for (let i=0; i < tabs.childNodes.length; i++ ) {
+            tabs.childNodes[i].removeAttribute("multiselect");
+        }
+    },
+
     /*** Event handlers ***/
 
     handleEvent: function(aEvent) {
@@ -75,8 +104,14 @@ var VerticalTabs = {
         case 'TabOpen':
             this.onTabOpen(aEvent);
             return;
+        case 'TabSelect':
+            this.onTabSelect(aEvent);
+            return;
         case 'mouseup':
             this.onMouseUp(aEvent);
+            return;
+        case 'mousedown':
+            this.onMouseDown(aEvent);
             return;
         case 'dragover':
             TabbrowserTabs.onDragOver(aEvent);
@@ -88,9 +123,34 @@ var VerticalTabs = {
         this.initTab(aEvent.originalTarget);
     },
 
+    onTabSelect: function(aEvent) {
+       aEvent.target.removeAttribute("multiselect");
+    },
+
     onMouseUp: function(aEvent) {
         if (aEvent.target.getAttribute("id") == "verticaltabs-splitter") {
             this.onTabbarResized();
+        }
+    },
+
+    onMouseDown: function(aEvent) {
+        if (aEvent.target.localName != "tab") {
+            return;
+        }
+        if (aEvent.button != 0) {
+            return;
+        }
+        var tabs = document.getElementById("tabbrowser-tabs");
+
+        // Check for Ctrl+click (multiselection).  On the Mac it's
+        // Cmd+click which is represented by metaKey.  Ctrl+click won't be
+        // possible on the Mac because that would be a right click (button 2)
+        if (aEvent.ctrlKey || aEvent.metaKey) {
+            this.multiSelect(tabs.tabbrowser.selectedTab);
+        } else if (aEvent.shiftKey) {
+            this.multiSpanSelect(tabs.tabbrowser.selectedTab, aEvent.target);
+        } else {
+            this.clearMultiSelect();
         }
     }
 
