0c3426c5022322ccec429989d1a8bc37f3463f43
Philipp von Weitershausen <philipp@weitershausen.de>
2010-11-10 12:11:01 -0800

    No need for JARs inside XPIs anymore as Firefox 4 no longer extracts the XPI upon installation.

--- makexpi.sh
+++ makexpi.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 rm -Rf build release.xpi
 mkdir build
-
-cp chrome.manifest.rel build/chrome.manifest
-
-cp -R install.rdf defaults build
-zip -r build/verticaltabs.jar content skin locale
-
+cp -R install.rdf chrome.manifest content skin locale defaults build
 cd build
 zip -r ../release.xpi *
