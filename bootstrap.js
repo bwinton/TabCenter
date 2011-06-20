33ddd45f027b73258a1db4364a349a8ab6a16202
Philipp von Weitershausen <philipp@weitershausen.de>
2011-06-20 13:40:44 +0200

    Factor out window-based loading to Labs's utils.js

--- bootstrap.js
+++ bootstrap.js
@@ -1,3 +1,39 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Vertical Tabs.
+ *
+ * The Initial Developer of the Original Code is
+ * Philipp von Weitershausen.
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 
 Cu.import("resource://gre/modules/Services.jsm");
@@ -11,7 +47,13 @@ const DEFAULT_PREFS = {
   "browser.allTabs.previews": true
 };
 
-let unloaders = [];
+/**
+ * Load and execute another file.
+ */
+let GLOBAL_SCOPE = this;
+function include(src) {
+  Services.scriptloader.loadSubScript(src, GLOBAL_SCOPE);
+}
 
 function setDefaultPrefs() {
   let branch = Services.prefs.getDefaultBranch("");
@@ -32,72 +74,29 @@ function setDefaultPrefs() {
 
 function loadIntoWindow(win) {
   let vt = new VerticalTabs(win);
-  unloaders.push(vt.unload.bind(vt));
-}
-
-function runOnLoad(window, callback) {
-  window.addEventListener("DOMContentLoaded", function onLoad() {
-    window.removeEventListener("DOMContentLoaded", onLoad, false);
-    callback(window);
-  }, false);
-}
-
-function eachWindow(callback) {
-  let enumerator = Services.wm.getEnumerator("navigator:browser");
-  while (enumerator.hasMoreElements()) {
-    let win = enumerator.getNext();
-    if (win.document.readyState === "complete") {
-      callback(win);
-    } else {
-      runOnLoad(win, callback);
-    }
-  }
-}
-
-function windowWatcher(subject, topic) {
-  if (topic !== "domwindowopened") {
-    return;
-  }
-  let win = subject.QueryInterface(Ci.nsIDOMWindow);
-  // We don't know the type of the window at this point yet, only when
-  // the load event has been fired.
-  runOnLoad(win, function (win) {
-    let doc = win.document.documentElement;
-    if (doc.getAttribute("windowtype") == "navigator:browser") {
-      loadIntoWindow(win);
-    }
-  });
-}
-
-function registerResource(name, installPath) {
-  let resource = Services.io.getProtocolHandler("resource")
-                         .QueryInterface(Ci.nsIResProtocolHandler);
-  let alias = Services.io.newFileURI(installPath);
-  if (!installPath.isDirectory()) {
-    alias = Services.io.newURI("jar:" + alias.spec + "!/", null, null);
-  }
-  resource.setSubstitution(name, alias);
-  unloaders.push(function () {
-    resource.setSubstitution(name, null);
-  });
+  unload(vt.unload.bind(vt), win);
 }
 
 function startup(data, reason) {
-  setDefaultPrefs();
-  unloaders.push(function() {
-    Services.prefs.getDefaultBranch(PREF_BRANCH).deleteBranch("");
-  });
-
   AddonManager.getAddonByID(data.id, function(addon) {
-    registerResource(RESOURCE_HOST, data.installPath);
+    // Load helpers from utils.js
+    include(addon.getResourceURI("utils.js").spec);
+
+    setDefaultPrefs();
+    unload(function() {
+      Services.prefs.getDefaultBranch(PREF_BRANCH).deleteBranch("");
+    });
+
+    // Register the resource:// alias.
+    let resource = Services.io.getProtocolHandler("resource")
+                           .QueryInterface(Ci.nsIResProtocolHandler);
+    resource.setSubstitution(RESOURCE_HOST, addon.getResourceURI("."));
+    unload(function () {
+      resource.setSubstitution(RESOURCE_HOST, null);
+    });
     Cu.import("resource://verticaltabs/content/verticaltabs.js");
 
-    eachWindow(loadIntoWindow);
-
-    Services.ww.registerNotification(windowWatcher);
-    unloaders.push(function() {
-      Services.ww.unregisterNotification(windowWatcher);
-    });
+    watchWindows(loadIntoWindow, "navigator:browser");
   });
 };
 
@@ -105,13 +104,5 @@ function shutdown(data, reason) {
   if (reason == APP_SHUTDOWN) {
     return;
   }
-  unloaders.forEach(function(unload) {
-    unload();
-  });
-}
-
-function install() {
-}
-
-function uninstall() {
+  unload();
 }
